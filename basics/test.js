4d4b56f60a8e17e37e27d789857984f0f3369024
rkm717 <98317463+rkm717@users.noreply.github.com>
2025-05-25 15:47:25 +0000

    dataType

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,7 @@
-console.log("Hello from VS Code!");
+//console.log("Hello from VS Code!");
+
+const accountId = "43325543"
+let name = "Ranjeet Mishra"
+var emailId = "rkm717@gmail.com"
+bankName = "SBI"
+console.table([accountId,name,emailId,bankName])
